28d17a6a46285497e0d0e3e1d2d962f85c87b110
Terry L <yuxlin21@gmail.com>
2024-08-25 18:50:27 -0400

    adjust styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,29 @@ import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
-const {
-  PUBLIC_SANITY_PROJECT_ID,
-  PUBLIC_SANITY_DATASET
-} = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ""
+);
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sanity({
-    projectId: PUBLIC_SANITY_PROJECT_ID,
-    dataset: PUBLIC_SANITY_DATASET,
-    useCdn: false,
-    apiVersion: "2024-08-13",
-    studioBasePath: "/studio",
-  }), tailwind(), partytown(), react(), sitemap()],
+  integrations: [
+    sanity({
+      projectId: PUBLIC_SANITY_PROJECT_ID,
+      dataset: PUBLIC_SANITY_DATASET,
+      useCdn: false,
+      apiVersion: "2024-08-13",
+      studioBasePath: "/studio",
+    }),
+    tailwind(),
+    partytown(),
+    react(),
+    sitemap(),
+  ],
   output: "static",
-  site: 'https://kokosrescue.com'
+  site: "https://kokosrescue.com",
 });
